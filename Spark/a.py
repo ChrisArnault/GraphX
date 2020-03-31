94021d0d48585e561098b398df0a730497586c35
Chris Arnault <arnault@lal.in2p3.fr>
2020-03-31 16:16:12 +0200

    First example for Spark with GraphFrames

--- Spark/a.py
+++ Spark/a.py
@@ -1,19 +1,8 @@
-import matplotlib.pyplot as plt
-import matplotlib
-
-import numpy as np
-
-import sys
-import os
-import random
-import time
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql import SQLContext
 
-from graphframes import GraphFrame
 from graphframes.examples import Graphs
 
 spark = SparkSession.builder.appName("GraphX").getOrCreate()
@@ -21,9 +10,10 @@ spark.sparkContext.setLogLevel("ERROR")
 
 sqlContext = SQLContext(spark.sparkContext)
 
-g = Graphs(sqlContext).friends()  # Get example graph
+g = Graphs(sqlContext).friends()
 
 g.vertices.show()
+g.edges.show()
 
 
 
