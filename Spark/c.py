0f6210ad3ade7664cad7cd7fe83864d5ebc681a0
Chris Arnault <arnault@lal.in2p3.fr>
2020-04-02 10:58:50 +0200

    c.py> Create edges

--- Spark/c.py
+++ Spark/c.py
@@ -14,6 +14,7 @@ spark.sparkContext.setLogLevel("ERROR")
 sqlContext = SQLContext(spark.sparkContext)
 
 num_vertices = 1000
+num_edges = 1000
 distance_max = 0.1
 
 x = lambda : np.random.random()
@@ -26,7 +27,29 @@ v = sqlContext.createDataFrame([(v_id, x(), y()) for v_id in range(num_vertices)
         vertex.out_vertices = [v for v in random.sample(vertices, np.random.randint(0, np.sqrt(len(vertices)))) if vertex.dist(v) < distance_max]
 """
 
-e = sqlContext.createDataFrame([(v_id, x(), y()) for v_id in range(num_vertices)], ["src", "dst"])
+def edge_it(n, last):
+    v = 0
+    while v < n:
+        m = random.randint(0, int(10))
+        j = 0
+        while j < m:
+            j += 1
+            w = random.randint(0, n)
+            print(v, w)
+            if v > last:
+                break
+            yield (v, w)
+        if v > last:
+            break
+        v += 1
+
+
+
+s = [(v, w) for v, w in edge_it(1000000000, 10)]
+print(s)
+
+
+e = sqlContext.createDataFrame([(v_id, w_id) for v_id, w_id in edge_it(num_vertices, 10)], ["src", "dst"])
 
 # Create a GraphFrame
 g = graphframes.GraphFrame(v, e)
